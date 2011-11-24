c66b21098495c4bcdc7a82bbc9e60f718b69c6f4
Lionel Marmier <lionel.marmier@cpnv.ch>
2011-11-24 15:56:30 +0100

    First architecture to business Logic of application

---
+++ Business Logic/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+BusinessLogic::Application.config.secret_token = '7df77873ba445187a4946b51899ce6bccbd564fd1efa31b7d8ca68bf7bf68ff6e7d903b63fd2195590af4823da01ccfc1493f013d3a3332f6a7e10c8aa25269e'
