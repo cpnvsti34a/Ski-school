662d56395e41210a945f42cf4da14d4ea76f619a
Lionel Marmier <lionel.marmier@cpnv.ch>
2011-12-01 16:49:57 +0100

    First operational Application with call webservice bye REST

---
+++ Web/src/java/controller/UserController.java
@@ -0,0 +1,41 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package controller;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.mvc.AbstractController;
+import service.UserService;
+
+/**
+ *
+ * @author Lionel.MARMIER
+ */
+public class UserController extends AbstractController {
+    
+    private UserService userService;
+    private String users;
+    
+      public UserController() {
+          
+    }
+    
+
+    public void setUserService(UserService userService) {
+        this.userService = userService;
+    }
+
+    @Override
+    protected ModelAndView handleRequestInternal(
+            HttpServletRequest request,
+            HttpServletResponse response) throws Exception {
+                ModelAndView mv = new ModelAndView("userView");
+                mv.addObject("pass", userService.retour("http://localhost:3000/users/1.xml"));
+                return mv;
+    }
+    
+    
+}
