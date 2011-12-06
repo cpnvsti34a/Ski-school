39fac77ccd0ab66947d905526a3ab854f553b9ef
Gregory Torche <gregory.torche@cpnv.ch>
2011-12-06 08:37:10 +0100

    test

--- Business Logic/app/controllers/reservations_controller.rb
+++ Business Logic/app/controllers/reservations_controller.rb
@@ -7,6 +7,7 @@ class ReservationsController < ApplicationController
     respond_to do |format|
       format.html # index.html.erb
       format.json { render json: @reservations }
+	  format.xml { render xml: @reservations }
     end
   end
 
