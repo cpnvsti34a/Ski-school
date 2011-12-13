be45144a5c36037a98ecbb1f90d95d20ea6f1e1b
Gregory Torche <gregory.torche@cpnv.ch>
2011-12-13 12:06:26 +0100

    changes reservation

--- Web/src/java/controller/ReservationController.java
+++ Web/src/java/controller/ReservationController.java
@@ -8,36 +8,40 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import org.springframework.web.portlet.ModelAndView;
 import service.ReservationService;
-import service.UserService;
+
 
 /**
  *
  * @author gregory.torche
  */
-public class ReservationController extends AbstractMethodError {
+public class ReservationController extends AbstractMethodError{
+    
     private ReservationService reservationService;
     private Reservation reservation;
-
-    
-        
+       
       public ReservationController() {
-     
+          reservation = new Reservation();
     }
     
 
-    public void setUserService(UserService userService) {
+    public void setReservationService(ReservationService reservationService) {
         this.reservationService = reservationService;
     }
 
-    @Override
-    protected ModelAndView handleRequestInternal(
+    //@Override
+    /*protected ModelAndView add(
             HttpServletRequest request,
             HttpServletResponse response) throws Exception {
-                ModelAndView mv = new ModelAndView("userView");
+                ModelAndView mv = new ModelAndView();
+                mv.setViewName("reservation");
                 //mv.addObject("pass", userService.retour("http://localhost:3000/users/1.xml"));
                 //mv.addObject("pass", userService.create("id="+tab[0]));
-                mv.addObject("pass", reservationService.create(reservation.getAll()));
+                mv.addObject("pass", "teteteiotj");
+                //mv.addObject("pass", reservationService.create(reservation.getAll()));
                 return mv;
+    }*/
+    protected ModelAndView add(){
+    mv = new ModelAndView("reservationView","reservation",reservation);
     }
     
 }
---
+++ Web/web/WEB-INF/jsp/reservationView.jsp
@@ -0,0 +1,18 @@
+<%-- 
+    Document   : reservation
+    Created on : 13 déc. 2011, 10:53:54
+    Author     : gregory.torche
+--%>
+
+<%@page contentType="text/html" pageEncoding="UTF-8"%>
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+        <title>JSP Page</title>
+    </head>
+    <body>
+        Voici la réservation que vous venez de créer : <br /><br />
+        ${pass} <br/>
+    </body>
+</html>
